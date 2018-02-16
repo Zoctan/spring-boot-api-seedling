476b209d5d65231f7fcca3fccdd74af1e799557f
Unknown <752481828@qq.com>
2018-02-16 16:08:56 +0800

    u

--- src/main/java/com/zoctan/seedling/Application.java
+++ src/main/java/com/zoctan/seedling/Application.java
@@ -1,6 +1,7 @@
 package com.zoctan.seedling;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,10 +10,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
+import static com.zoctan.seedling.core.ProjectConstant.MAPPER_PACKAGE;
+
 @EnableEncryptableProperties
 @SpringBootApplication
 @EnableCaching
 @EnableTransactionManagement
+@MapperScan(basePackages = MAPPER_PACKAGE)
 public class Application {
     @PostConstruct
     void started() {
